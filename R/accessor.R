832fad1568ca783f87359dca5706700b44b98b9f
Leslie Myint <leslie.myint@gmail.com>
2016-09-25 14:50:30 -0400

    Accessor functions

---
+++ R/accessor.R
@@ -0,0 +1,39 @@
+files <- function(obj) {
+    obj@files
+}
+
+classes <- function(obj) {
+    obj@classes
+}
+
+rawpeakinfo <- function(obj) {
+    obj@rawpeakinfo
+}
+
+mzParams <- function(obj) {
+    obj@mzParams
+}
+
+densityEstimate <- function(obj) {
+    obj@dens
+}
+
+densityCutoff <- function(obj) {
+    obj@dcutoff
+}
+
+densityQuantiles <- function(obj) {
+    obj@densquants
+}
+
+peakInfo <- function(obj) {
+    obj@blobs
+}
+
+peakQuants <- function(obj) {
+    obj@quants
+}
+
+diffrep <- function(obj) {
+    obj@diffrep
+}
