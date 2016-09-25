9d8600f1be25b06c778f6ac7db4332226d5d8035
Leslie Myint <leslie.myint@gmail.com>
2016-09-25 14:13:43 -0400

    Don't need rtwarp global var

--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,2 @@
 utils::globalVariables(c("blobnum", "mzmin", "mzmax", "weight", "bg", "gmz",
-                         "xic", "scanmax", "scanmin", "scanorig", "N", "rtwarp"))
+                         "xic", "scanmax", "scanmin", "scanorig", "N"))
