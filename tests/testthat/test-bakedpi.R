429cb029f9781462aafc27755d31b3c81afc5e68
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2016-10-13 10:04:57 -0400

    adding testing for background correction

---
+++ tests/testthat/test-bakedpi.R
@@ -0,0 +1,9 @@
+library(yamss)
+context("Testing Background Correction")
+load(file.path(path.package("yamss"), "scripts", "digesst.rda"))
+data(cmsRawExample)
+background <- yamss:::backgroundCorrection(cmsRawExample)
+test_that("Background correction", {
+    expect_equal(yamss:::.digestDataTableRaw(yamss:::.rawDT(background)), digests$backgroundRaw)
+    expect_equal(yamss:::.digestDataTableBG(yamss:::.bgcorrDTDT(background)), digests$backgroundCorr)
+    })
